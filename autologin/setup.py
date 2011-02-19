b963db3e3674ded9a04e484ac96b7fafbcb94d69
mhweaver <mhweaver@gmail.com>
2011-02-19 15:20:48 +0300

    Added autologin files

---
+++ autologin/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+import py2exe
+import sys
+
+#setup(console=['autologin.py'])
+#if len(sys.argv) == 2:
+#    entry_point = sys.argv[1]
+#    sys.argv.pop()
+#    sys.argv.append('py2exe')
+#    sys.argv.append('-q')
+#else:
+#    print 'usage: compile.py <python_script>\n'
+#    raw_input("press ENTER to exit...")
+#    sys.exit(1) 
+
+opts = {
+    'py2exe': {
+        'compressed': 1,
+        'optimize': 2,
+        'ascii': 1,
+        'bundle_files': 1
+    }
+}
+
+setup(console=['autologin.py'], options=opts)
